4d90b608a79277897739a7a65500b71c7cc30b2e
jsdiddon <justin@delaris.com>
2014-05-19 08:13:37 -0700

    added pipes to differentiate data points, need to still figure out newlines.

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+
+
+
+
+
+
+
+
 var
 	express = require('express')
 	, app = express()
@@ -63,21 +71,31 @@ http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 
 	sp.open(function() {
-		console.log("open");
+		console.log("Serial Port Open");
+		var readyCounter = 0;
+
+		// On data event
 		sp.on('data', function (allData) {
-			console.log('data');
+			/*if(allData == 1 && readyCounter != 1) {
+				readyCounter = 1;
+				console.log("Ready!");
+			} else if(readyCounter == 1) {
+			*/
+				/* Take raw data from sensor and split data points on "/" and turn it into an
+				array of data.*/
+				data.parse(allData, function(dataArray) {
+					//console.log("dataArray " + dataArray);
+					data.log(dataArray, function(err, fileNameLoc) {
+						if (err) throw(err);
 
-			data.parse(allData, function(dataArray) {
-				console.log(dataArray);
-				data.log(dataArray, function(err, fileNameLoc) {
-					if (err) throw(err);
+						data.convert(fileNameLoc, function(err, savedFile) {
+							console.log(savedFile);
+			//			readyCounter = 0;
+						});
 
-					data.convert(fileNameLoc, function(err, savedFile) {
-						console.log(savedFile);
 					});
-
 				});
-			});
+	//	}
 
 		});
 	});
--- readme.md
+++ readme.md
@@ -2,3 +2,8 @@ flinch
 ======
 
 flinch
+
+
+Notes
+3-18-14
+Added pipes to seperate data elements on a data point. Now need to figure out how to add newline to each element.
